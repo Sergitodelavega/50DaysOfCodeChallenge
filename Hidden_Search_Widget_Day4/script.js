ad2d346f9de27f1fe6f6bcfcc4f387b0316529dd
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-01 08:00:52 +0100

    Modified Js file Day 4

--- Hidden_Search_Widget_Day4/script.js
+++ Hidden_Search_Widget_Day4/script.js
@@ -2,7 +2,7 @@ const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
 const search = document.querySelector('.search');
 
-btn.addEventListener('click', ()=> {
+btn.addEventListener('click', () => {
     search.classList.toggle('active');
     input.focus();
 })
