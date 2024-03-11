ca6133f46fc4a8ca6fbfa634d6a6941e9e82b2f6
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 13:49:30 +0100

    Add Comment

--- Expanding_Cards_Day1/script.js
+++ Expanding_Cards_Day1/script.js
@@ -1,12 +1,17 @@
+// Select all elements with the class 'panel'
 const panels = document.querySelectorAll('.panel');
 
+// Add a click event listener to each panel
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        // When a panel is clicked, remove the 'active' class from all panels
         removeActiveClasses();
+        // And add the 'active' class to the clicked panel
         panel.classList.add('active');
     });
 });
 
+// Function to remove the 'active' class from all panels
 function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active');
