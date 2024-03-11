2cbc95d847044130d540583831109c42418d7973
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 13:59:53 +0100

    Add Comment

--- Blurry_Loading_Day5/script.js
+++ Blurry_Loading_Day5/script.js
@@ -3,27 +3,29 @@ const loadText = document.querySelector('.loading-text');
 
 let load = 0;
 
+// Set an interval to call the burring function every 30 milliseconds
 let int = setInterval(burring, 30);
 
+// Define the burring function
 function burring(){
     load++;
 
+    // If the load value is greater than 99, clear the interval
     if(load > 99){
         clearInterval(int);
     }
 
+    // Update the loading text with the current load value
     loadText.innerHTML = `${load}%`;
-    loadText.style.opacity = scale(load, 0, 100, 1, 0);
 
+    // Scale he opacity of the loading text and the blur value of the background based on the current load value
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
     bg.style.filter = `blur(${scale(load, 0, 100, 30,  0)}px)`;
 } 
 
-
-
-
-
-
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+//Define the scale function to map a range of numbers to another range of numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
   }
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
