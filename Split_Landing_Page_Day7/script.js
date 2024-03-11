ae307f2aae04171ee8d78b6e7b606cb1c4d16a36
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 14:05:22 +0100

    Add Comment

--- Split_Landing_Page_Day7/script.js
+++ Split_Landing_Page_Day7/script.js
@@ -2,9 +2,14 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
 
+// Add a 'mouseenter' and 'mouseleave event listener to the left element
+// Add and remove the 'hover-left' class to the container element
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
 
+
+// Add a 'mouseenter' and 'mouseleave event listener to the right element
+// Add and remove the 'hover-right' class to the container element
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
 
