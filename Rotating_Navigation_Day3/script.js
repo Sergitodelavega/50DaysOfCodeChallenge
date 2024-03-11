ee0021afe69cfe1853907eb61aa4483e06d58433
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 13:52:47 +0100

    Add Comment

--- Rotating_Navigation_Day3/script.js
+++ Rotating_Navigation_Day3/script.js
@@ -2,7 +2,11 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
 
+// Add a click event listener to the 'open' button  
+// Add the 'show-nav' class to the container element
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
+// Add a click event listener to the 'close' button
+// Remove the 'show-nav' class from the container element
 close.addEventListener('click', () => container.classList.remove('show-nav'));
 
