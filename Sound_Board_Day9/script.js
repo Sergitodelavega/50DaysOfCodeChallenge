c4e0d7cc3284d31ab16a69f0c22cfbc2faf9f440
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 14:10:22 +0100

    Add Comment

--- Sound_Board_Day9/script.js
+++ Sound_Board_Day9/script.js
@@ -1,5 +1,6 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
+// Loop through each sound in the sounds array
 sounds.forEach(sound => {
     const btn = document.createElement('button');
 
@@ -8,6 +9,7 @@ sounds.forEach(sound => {
     btn.innerText = sound;
 
     btn.addEventListener('click', () => {
+        // Call the stopSongs function to stop any currently playing songs
         stopSongs();
 
         document.getElementById(sound).play();
@@ -18,6 +20,7 @@ sounds.forEach(sound => {
 
 });
 
+// Define the stopSongs function
 function stopSongs(){
     sounds.forEach(sound => {
         song = document.getElementById(sound);
