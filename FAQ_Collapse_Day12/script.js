c6124177115887004742f4db775c7f69d691a54d
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 14:18:27 +0100

    Add Comment

--- FAQ_Collapse_Day12/script.js
+++ FAQ_Collapse_Day12/script.js
@@ -1,7 +1,10 @@
 const toggles = document.querySelectorAll('.faq-toggle');
 
+// Loop through each toggle element and add a click event listener
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
+        
+        // Toggle the 'active' class on the parent node of the toggle element
         toggle.parentNode.classList.toggle('active');
     });
 });
