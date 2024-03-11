7c29953b227bf77e11a95f9441a16f14e8732c13
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 13:56:56 +0100

    Add Comment

--- Hidden_Search_Widget_Day4/script.js
+++ Hidden_Search_Widget_Day4/script.js
@@ -2,7 +2,11 @@ const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
 const search = document.querySelector('.search');
 
+// Add a click event listener to the button 
 btn.addEventListener('click', () => {
+    // Toggle the 'active' class on the search element
     search.classList.toggle('active');
+
+    // Set the focus on the input element
     input.focus();
 })
