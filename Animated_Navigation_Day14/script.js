defdbcf0303d1db2f3e25278017f5ae512da3084
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 14:22:53 +0100

    Add Comment

--- Animated_Navigation_Day14/script.js
+++ Animated_Navigation_Day14/script.js
@@ -1,6 +1,7 @@
 btnEl = document.getElementById('toggle');
 nav = document.getElementById('nav');
 
+// Add a click event listener to the btnEl
 btnEl.addEventListener( 'click', () => {
     nav.classList.toggle('active');
 })
