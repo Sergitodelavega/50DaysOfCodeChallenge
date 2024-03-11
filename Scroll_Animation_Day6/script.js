b55a4f0b4ac4b04631143fd65ce76c844508f671
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-03-11 14:02:15 +0100

    Add Comment

--- Scroll_Animation_Day6/script.js
+++ Scroll_Animation_Day6/script.js
@@ -1,14 +1,18 @@
 const boxes = document.querySelectorAll('.box');
 
+// Add a scroll event listener to the window object
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
 
+// Define the checkBoxes function
 function checkBoxes(){
+    // Calculate the trigger point for the boxes to become visible
     const triggerBottom = window.innerHeight / 5 * 4;
     console.log(triggerBottom);
 
     boxes.forEach(box => {
+        // Get the top position of the current box relative to the viewport
         boxTop = box.getBoundingClientRect().top;
         
         if(boxTop < triggerBottom){
